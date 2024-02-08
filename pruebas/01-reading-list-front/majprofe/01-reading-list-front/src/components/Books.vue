42c8ae2a81a7e58e56e447f2c748d4775cdd0ba9
majprofe <marrjua393@g.educaand.es>
2024-02-08 16:42:15 +0100

    Finalizado

--- pruebas/01-reading-list-front/majprofe/01-reading-list-front/src/components/Books.vue
+++ pruebas/01-reading-list-front/majprofe/01-reading-list-front/src/components/Books.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, ref, watch, watchEffect } from 'vue'
+import type { Ref } from 'vue'
 import data from '../books.json'
 type Book = {
     title: string;
@@ -15,6 +16,8 @@ type Book = {
     };
 };
 const books: Book[] = data.library.map((data) => data.book);
+const genres: string[] = Array.from(new Set(books.map((book) => book.genre))) 
+
 
 const genre = ref('');
 const handleChanges = (event: Event) => {
@@ -23,6 +26,8 @@ const handleChanges = (event: Event) => {
     console.log(genre.value);
 }
 
+const readList: Ref<string[]> = ref([])
+
 const matches = computed(() =>{
     if(genre.value === ''){
         return books
@@ -30,22 +35,39 @@ const matches = computed(() =>{
     return books.filter((book) => book.genre === genre.value)
 })
 
+const handleBookClick = (book: string) => {
+    readList.value = readList.value.includes(book) ? readList.value.filter((isbn) => isbn !== book) : [...readList.value, book]
+    localStorage.setItem('readList', JSON.stringify(readList.value))
+}
+
+watchEffect(() => {
+    readList.value = JSON.parse(localStorage.getItem('readList') || '[]')
+})
+
+// Escuchar cambios en el LocalStorage en todas las pestañas
+window.addEventListener('storage', (event) => {
+    if (event.key === 'readList') {
+        readList.value = JSON.parse(event.newValue || '[]');
+    }
+});
+
 </script>
 
 <template>
-    <article class="grid gad-4">
+    <article class="grid gap-4">
         <nav>
             <select  @change="handleChanges">
                 <option value="">Todos</option>
-                <option value="Fantasía">Fantasía</option>
-                <option value="Ciencia ficción">Ciencia ficción</option>
-
+                <option v-for="genre in genres" :key="genre" :value="genre">{{ genre }}</option>
             </select>
         </nav>
         <ul class="grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4">
-            <li v-for="book in matches" :key="book.ISBN">
+            <li v-for="book in matches" :key="book.ISBN" class="grid gap-2" @click="handleBookClick(book.ISBN)">
                 <img class="aspect-[9/14] object-cover" :src="book.cover" :alt="book.title">
-                <p>{{ book.title }}</p>
+                <p>
+                    <span v-if="readList.includes(book.ISBN)">⭐</span>
+                    {{ book.title }}
+                </p>
             </li>
         </ul>
     </article>
